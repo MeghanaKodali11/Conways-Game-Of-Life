539a3917f66212d27cbed7a5278d1b16225d6ae1
MeghanaKodali11 <61222712+MeghanaKodali11@users.noreply.github.com>
2020-04-21 21:29:42 -0400

    Create RegisterForm.php

---
+++ RegisterForm.php
@@ -0,0 +1,115 @@
+<html>
+<head>
+<title>Game of Life-Registration</title>
+<link rel="stylesheet" type="text/css" href="./style1.css" title="style" />
+<style>
+input[type=text],input[type=password]{
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}</style>
+
+</head>
+
+<body background="back.jpg" style="margin-top:0px">
+
+
+
+<?php
+
+function test_input($data) 
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+		
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+
+$fname= test_input($_POST["fname"]);
+$lname= test_input($_POST["lname"]);
+$uid= test_input($_POST["uid"]);
+$pwd= test_input($_POST["pwd"]);
+
+
+$servername = "localhost";
+$username = "mkodali1";
+$password = "mkodali1";
+$dbname = "mkodali1";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) 
+{
+   echo "<script type='text/javascript'>alert('Server Connection is failed')</script>"; 
+} 
+
+$sql = "INSERT INTO login_ids (firstname, lastname, username, password)
+VALUES ('".$fname."','".$lname."','".$uid."', '".$pwd."')";
+
+if ($conn->query($sql) === TRUE) 
+{
+    echo "<script type='text/javascript'>alert('Registration Successfull!')</script>"; 
+}
+else 
+{
+    echo "<script type='text/javascript'>alert('Connection Failed: '". $conn->error.")</script>"; ;
+}
+
+$conn->close();
+}
+?>
+
+<a name= "Register"></a>
+<img src="head.png" height="150" width="400" alt=""/>
+<h2>User Registration Form</h2>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		<br>
+		<table align="center" style="
+    width: 560px;
+    height: 350px;">
+			<tr>
+				<td style="font-family: cursive; font-weight: bold">First Name :</td>
+				<td><input type="text" name="fname" placeholder="Your Firstname.." required></td>
+			</tr>
+			<br>
+			<tr>
+				<td style="font-family: cursive; font-weight: bold">Last Name :</td>
+				<td><input type="text" name="lname" placeholder="Your Lastname.." required></td>
+			</tr>
+			<br>
+			<tr>
+				<td style="font-family: cursive; font-weight: bold">User ID :</td>
+				<td><input type="text" name="uid" placeholder="Your UserID.." required></td>
+			</tr>
+			<br>
+			<tr>
+				
+				<td style="font-family: cursive; font-weight: bold">Password :</td>
+				<td><input type="password" name="pwd" placeholder="Your Password.." required></td>
+			</tr>
+			<tr>
+				<td> </td>
+				<td><input type="submit" value="Register" class="myButton" ></td>
+			</tr>
+			<tr>
+				<td> </td>
+				<td>
+				<a href="LoginForm.php" class="myButton" >Login</a>
+				
+				</td>
+			</tr>
+			
+		</table>
+	</form>
+
+</body>
+</html>
